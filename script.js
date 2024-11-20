b8b46183ff1a46d614385382132384240ba0d2db
ZeoxRY <ristezlatev6@gmail.com>
2024-11-20 16:50:40 +0100

    Async and await

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-fetch('https://dog.ceo/api/breeds/image/random')
-    .then((response) => response.json())
-    .then(json =>{
-        console.log(json);
-        const image = document.getElementById('picture');
-       image.src = json.message;
-       const button = document.getElementById('newPic');
-       button.addEventListener('click', function(){
-        fetch('https://dog.ceo/api/breeds/image/random')
-        .then((response) => response.json())
-        .then(json =>{
-        console.log(json);
-        image.src = json.message;
-        });
-       });
-    }
+async function generateIMG(){
+const response = await fetch("https://dog.ceo/api/breeds/image/random");
+const json = await response.json();
 
-)
-    
+console.log(json);
+const image = document.getElementById("picture");
+image.src = json.message;
+const button = document.getElementById("newPic");
+button.addEventListener("click", async function () {
+  const updated = await fetch("https://dog.ceo/api/breeds/image/random");
+  const updatedjson = await updated.json();
+  image.src = updatedjson.message;
+});
+}
+
+generateIMG();
